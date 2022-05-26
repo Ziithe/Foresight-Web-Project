88cec5fb4a3834fbfb2061020411b20385ca9e14
Ziithe <z.hiwa@alustudent.com>
2022-05-26 12:39:22 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ export default function App() {
     <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
       <div className='text-center p-3 text-light' style={{ backgroundColor: 'rgba(33,137,126)' }}>
         &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-light' href='#'>
-          Foresight Agricultural Insights
+        <a className='text-light' href='www.ziitheewen.com'>
+          Ziithe Ewen Hiwa
         </a>
       </div>
     </MDBFooter>
